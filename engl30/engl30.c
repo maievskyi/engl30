5304c51c61cb01581ad8514ee3d93723c8e34505
maievskyi <maevua2@gmail.com>
2023-06-01 21:12:16 +0300

    перше main()

---
+++ engl30/engl30.c
@@ -0,0 +1,41 @@
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+#define _CRT_SECURE_NO_WARNINGS  //   ?? подробнее об предупреждениях ??
+
+#include<stdlib.h>
+#include<stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <Windows.h>
+#include"engl30.h"  //
+
+
+
+
+
+
+
+
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       main     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
+int main(int argc, const char ** argv, const char** env)
+{
+
+	system("chcp 1251 > nul");
+
+
+
+
+
+	system("pause");
+}  //end main
---
+++ engl30/engllib30.c
@@ -0,0 +1,14 @@
+//
+//
+//
+//
+//
+
+
+
+
+#include<stdlib.h>
+#include<stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include"engl30.h"  //
