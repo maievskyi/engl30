2145c9e782bcb6156a739c54e4dbe3a114fcd3a0
maievskiy <maevua@gmail.com>
2023-06-12 14:09:09 +0300

    гмз - начало с pFcommon

--- engl30/engl30.c
+++ engl30/engl30.c
@@ -28,6 +28,8 @@ FILE *pFsort;	//---> указатель на структ. ф в котором 
 FILE *pFfreqsort;//---> указ на  ф в котор сохранена частотно-сорт базу слов
 
 
+struct inidat *pcommon;	//--->указ на ДИН пам общей базы слов с перев
+struct inidat *psettings;	//--->указ на ДИН пам базы ini имён и настроек прог-мы
 
 
 
@@ -39,7 +41,71 @@ int main(int argc, const char ** argv, const char** env)
 	errno_t err;			//  переменная (int?) для вывода ошибок ? внутр переменная (int?)
 
 
-	struct inidat *psettings;	//--->указ на ДИН пам базы ini имён и настроек прог-мы
+//________ открытие файла общей базы слов, или при отсутствии - созд новой
+	err = fopen_s(&pFcommon, "commondictionary.dat", "r+b");	 // открывается ли на ЧТЕНИЕ с дозаписью
+	if (err)	// НЕТ  СТАРОГО файла общей базы слов "r+b" = нет. Делаем новый = "w+b"							
+	{			// (!!! только "r" в VS чтото не получилось)
+		perror("commondictionary.dat");
+		puts("\n !!! НЕ найден файл commondictionary.dat \n  нажмите 'y или n'\n чтобы создать новый\n");
+		system("pause");
+		if ('n' == _getch(stdin))//   ============
+		{
+			exit(1);
+		}
+		else
+		{
+			// значит открывается НОВЫЙ INI commondictionary.dat "w+b" на ЗАПИСЬ-ЧТЕНИЕ
+			err = fopen_s(&pFcommon, "commondictionary.dat", "w+b");
+			if (err)	//ошибка выхол почему-то  НЕ СОЗДАЛСЯ НОВЫЙ fini.dat
+			{
+				puts("\n !!! почемуто не создался новый commondictionary.dat \n");
+				perror("commondictionary.dat");
+				//free(psettings);				//освоб памяти
+				system("pause");
+				exit(1);					// выход урок сорвался
+			}
+			puts("\n  открывается НОВЫЙ commondictionary.datcommondictionary.dat \"w + b\" на ЗАПИСЬ-ЧТЕНИЕ \n");
+
+		} // end else
+	} // end if (err)
+//__________________________________________________________________________________________
+		/*
+		//// значит открывается НОВЫЙ INI fini.dat "w+b" на ЗАПИСЬ-ЧТЕНИЕ
+		err = fopen_s(&pFini, "fini.dat", "w+b");
+		if (err)	//ошибка выхол почему-то  НЕ СОЗДАЛСЯ НОВЫЙ fini.dat
+		{
+		puts("\n !!! почемуто не создался новый ini файл пользователя \n");
+		perror("fini.dat");
+		free(psettings);				//освоб памяти
+		system("pause");
+		exit(1);					// выход урок сорвался
+		}
+		else
+		{	// открылся НОВЫЙ fini.dat на ЗАПИСЬ-ЧТЕНИЕ
+		// (^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^)
+		// ПОПОЗЖЕ тут дописать в дин пам psettings настр по умолчанию !!! ^^^^^^
+		//psettings->sorttype = 1; // по умолч алфав сортировка
+		//strncpy(psettings->ininamenosortf, TEXTIN"_nosort.dat", EN1); // имя несорт ф
+		//strncpy(psettings->ininamealphsortf, TEXTIN"_alphsort.dat", EN1);// имя алфав сорт ф
+		//strncpy(psettings->ininameafreqsortf, TEXTIN"_freqalphsort.dat", EN1);//имя част-алфав ф
+		//psettings->inicountsepword = 0; // по умолч отсепарировано
+		//psettings->inicountnumword = 0; // по умолч  отсортированноо с уменьшением еол слов
+		//запись  стандартн по умлч настроек ИЗ ДИН ПАМЯТИ ?pmeminidat? в ф fini.dat  ======
+		size_t result = fwrite(psettings, sizeof(struct inidat), QUANTITYNAME, pFini);
+		//puts("\n +++  создался  НОВЫЙ ini файл пользователя со стнд настр \n");	//debug
+		puts("\n Записан из Д ПАМ на HDD Новый стандартный файл пользователя \
+		с стнд настр\"fini.dat\" \n");
+
+		fclose(pFini);	// закрыть ф-й поток в который записаны настройки ini по умолчанию
+		//puts("\n +++  создался  НОВЫЙ ini файл пользователя со стнд настр \n");	//debug
+		}
+//____________________________________________________________________________
+
+
+
+
+
+
 
 	//1)создание  дин памяти psettings
 	psettings = (struct inidat*)malloc(sizeof(struct inidat)); //созд ДИН пам под стркт
@@ -62,7 +128,7 @@ int main(int argc, const char ** argv, const char** env)
 	psettings->inicountsepword = 0; // копия общего кол countnosort отсепарир-х слов в тексте
 	psettings->inicountnumword = 0; // копия общего кол countnosort отсепарир-х слов в тексте
 	*/
-	//______________________________________________						
+	//_________________________________________________________________________________________						
 
 
 	//2) есть ли ф fini.dat  ==============================================================
@@ -139,7 +205,6 @@ int main(int argc, const char ** argv, const char** env)
 	}
 
 
-
-	free(psettings);//оcвоб пам с настройками из ф ini
+	fclose(pFcommon);//оcвоб пам с настройками из ф ini
 	system("pause");
 }  //end main
